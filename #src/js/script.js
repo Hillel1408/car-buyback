46cb01292db63508a8d3993ff4fb120be7e77f42
Hillel1408 <etemax@bk.ru>
2023-10-30 20:09:10 +0300

    car-buyback

--- #src/js/script.js
+++ #src/js/script.js
@@ -5,3 +5,13 @@ const swiper = new Swiper(".cars__slider", {
     clickable: true,
   },
 });
+
+const swiper2 = new Swiper(".latest__slider", {
+  direction: "horizontal",
+  slidesPerView: 4,
+  spaceBetween: 25,
+  navigation: {
+    nextEl: ".latest__button-next",
+    prevEl: ".latest__button-prev",
+  },
+});
